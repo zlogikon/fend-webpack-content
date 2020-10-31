3a389778e96b36e3f3acd2991e6073b689a10954
Andrew Coulon <zlogik@outlook.com>
2020-10-31 11:35:41 -0400

    add port

--- src/server/index.js
+++ src/server/index.js
@@ -12,9 +12,11 @@ app.get('/', function (req, res) {
     res.sendFile('dist/index.html')
 })
 
+const port = 8050;
+
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(port, function () {
+    console.log('Example app listening on port: ' + port)
 })
 
 app.get('/test', function (req, res) {
